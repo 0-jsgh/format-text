032f57b0d3c264e95bb909deef8fbded80e08d23
Johan Gomez <142572116+0-jsgh@users.noreply.github.com>
2024-03-20 17:16:32 -0500

    Update script-02.js

--- meta/script-02.js
+++ meta/script-02.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var head = document.head;
 
+  var style = document.createElement('style');
+      style.id = 'customStyle';
+          style.innerHTML = '.cambio{display:none;}';
+
+      // Agregar la etiqueta <style> al <head>
+  document.head.appendChild(style);
+};
