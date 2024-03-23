d2d3e6ab18e486f912e2a8c3f914c08767467c24
Johan Gomez <142572116+0-jsgh@users.noreply.github.com>
2024-03-23 00:42:23 -0500

    Delete meta/script-02.js

--- meta/script-02.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-// Obtener todos los elementos con la clase 'cambio'
-  var enlacesCambio = document.querySelectorAll('.cambio');
-  // Eliminar los elementos obtenidos
-  enlacesCambio.forEach(function(enlace) {
-    enlace.parentNode.removeChild(enlace);
-  });
-});
