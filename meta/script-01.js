8aafa191b59d082184de45bfbbdf9981ceab93aa
Johan Gomez <142572116+0-jsgh@users.noreply.github.com>
2024-03-23 00:39:07 -0500

    Update script-01.js

--- meta/script-01.js
+++ meta/script-01.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Obtener todos los elementos con la clase 'cerrar'
-  var enlacesCerrar = document.querySelectorAll('.cerrar');
+  var enlacesCerrar = document.querySelectorAll('.menu');
   // Eliminar los elementos obtenidos
   enlacesCerrar.forEach(function(enlace) {
     enlace.parentNode.removeChild(enlace);
